6f4777d1b4ffdc662c98e67240879ef6fe67858a
Lobzang Yonten <Lobzangyenten1@gmail.com>
2023-10-11 23:44:39 +0600

    added loginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
-import 'package:examcellapp/views/HomePage.dart';
+import 'package:examcellapp/firebase_options.dart';
+import 'package:examcellapp/views/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,12 +18,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Homepage(),
+      home: const SplashScreen(),
     );
   }
 }
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,15 +1,83 @@
+import 'package:examcellapp/services/auth/auth_service.dart';
+import 'package:examcellapp/views/HomePage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget {
-  const LoginView({super.key});
+  const LoginView({Key? key}) : super(key: key);
 
   @override
   State<LoginView> createState() => _LoginViewState();
 }
 
 class _LoginViewState extends State<LoginView> {
+  final FirebaseAuthService _auth = FirebaseAuthService();
+
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                controller: _email,
+                enableSuggestions: false,
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  hintText: 'email',
+                ),
+              ),
+              TextField(
+                controller: _password,
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: const InputDecoration(
+                  hintText: 'password',
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final email = _email.text;
+                  final password = _password.text;
+
+                  User? user =
+                      await _auth.signInWithEmailAndPassword(email, password);
+
+                  if (user != null) {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const Homepage()));
+                  }
+                },
+                child: const Text("LogIn"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
