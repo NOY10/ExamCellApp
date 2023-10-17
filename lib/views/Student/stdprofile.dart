dd2b4e632d2ff9ecebc9e3e4bf69316be27ca884
N0rphel <tnorphel09@gmail.com>
2023-10-17 20:07:25 +0600

    shortened the studentProfile Code

--- lib/views/Student/stdprofile.dart
+++ lib/views/Student/stdprofile.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+final List<String> accountDetail = [
+  'Tshering Norphel',
+  '02210233',
+  'BE Information Technology',
+  'Year Third'
+];
+
+final List<String> perDetail = [
+  '10710002711',
+  '21/09/2001',
+  'Male',
+  '02210233.cst@rub.edu.bt'
+];
+
 class StudentProfile extends StatelessWidget {
   const StudentProfile({super.key});
   final rectColor = 0xFFE7E6E6;
@@ -19,228 +33,118 @@ class StudentProfile extends StatelessWidget {
   }
 
   Container personalDetail(BuildContext context) {
-    return Container(
-          color: Colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8),
-                    
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                          Text(
-                          'Personal Detail',
-                          style: TextStyle(
-                            color: Color(textColor),
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16
-                          ),
-                        ),
-                        const SizedBox(height: 5,),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.blue),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left : 8.0),
-                            child: Text(
-                              '10210002711',
-                              style: TextStyle(
-                                color: Color(textColor),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 16
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 5,),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.blue),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left : 8.0),
-                            child: Text(
-                              '21/09/2001',
-                              style: TextStyle(
-                                color: Color(textColor),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 16
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 5,),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.blue),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left : 8.0),
-                            child: Text(
-                              'Male',
-                              style: TextStyle(
-                                color: Color(textColor),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 16
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 5,),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.blue),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left : 8.0),
-                            child: Text(
-                              '02210233.cst@rub.edu.bt',
-                              style: TextStyle(
-                                color: Color(textColor),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 16
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 5,),
-                        
-                      ],
+  return Container(
+    color: Colors.white,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Personal Detail',
+                    style: TextStyle(
+                      color: Color(textColor),
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
                     ),
                   ),
-                ),
+                  const SizedBox(height: 5,),
+                  ListView.separated(
+                    shrinkWrap: true,
+                    itemCount: perDetail.length,
+                    itemBuilder: (context, index) => Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blue),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          perDetail [index],
+                          style: TextStyle(
+                            color: Color(textColor),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                    separatorBuilder: (context, index) => const SizedBox(height: 8,),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        );
-  }
+        ),
+      ],
+    ),
+  );
+}
 
   Container accDetail(BuildContext context) {
-    return Container(
-          color: Colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8),
-                    
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                          Text(
-                          'Account Detail',
-                          style: TextStyle(
-                            color: Color(textColor),
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16
-                          ),
-                        ),
-                        const SizedBox(height: 5,),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.blue),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left : 8.0),
-                            child: Text(
-                              'Tshering Norphel',
-                              style: TextStyle(
-                                color: Color(textColor),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 16
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 5,),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.blue),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left : 8.0),
-                            child: Text(
-                              '02210233',
-                              style: TextStyle(
-                                color: Color(textColor),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 16
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 5,),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.blue),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left : 8.0),
-                            child: Text(
-                              'BE Information Technology',
-                              style: TextStyle(
-                                color: Color(textColor),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 16
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 5,),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.blue),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left : 8.0),
-                            child: Text(
-                              'Year Third',
-                              style: TextStyle(
-                                color: Color(textColor),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 16
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 5,),
-                        
-                      ],
+  return Container(
+    color: Colors.white,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Account Detail',
+                    style: TextStyle(
+                      color: Color(textColor),
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
                     ),
                   ),
-                ),
+                  const SizedBox(height: 5,),
+                  ListView.separated(
+                    shrinkWrap: true,
+                    itemCount: accountDetail.length,
+                    itemBuilder: (context, index) => Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blue),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          accountDetail [index],
+                          style: TextStyle(
+                            color: Color(textColor),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                    separatorBuilder: (context, index) => const SizedBox(height: 8,),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        );
-  }
+        ),
+      ],
+    ),
+  );
+}
 
   Padding banner(BuildContext context) {
     return Padding(
