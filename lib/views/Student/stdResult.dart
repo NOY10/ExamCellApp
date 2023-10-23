2569b2f43de5e426ac89ddd393fe0522d87a93d1
Phurptshering <phurpatsering329@gmail.com>
2023-10-24 00:37:26 +0600

    dynamic navbar and appbar

--- lib/views/Student/stdResult.dart
+++ lib/views/Student/stdResult.dart
@@ -3,18 +3,71 @@ import 'package:flutter/material.dart';
 class StudentResult extends StatelessWidget {
   const StudentResult({super.key});
 
-  final FONT_SIZE = 12;
   final TEXT_COLOR = 0xFF1A1717;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Result'),
-        centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 39, 121, 189),
+      body: Column(
+        children: [
+          const SizedBox(height: 16), // space at the top of the Column
+
+          // First Row with a photo, text, and another photo
+          Padding(
+            padding: const EdgeInsets.only(top: 5.0), // space from the top
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // RUB photo
+                Container(
+                  width: 80,
+                  height: 80,
+                  child: Image.asset('assets/images/RUB_logo.png'),
+                ),
+                SizedBox(width: 40), // space between the elements
+                // Text
+                Container(
+                  width: 140,
+                  child: Column(
+                    children: [
+                      Text(
+                        'College of Science and Technology',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Color(TEXT_COLOR),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'ཚན་རག་དང་འཕྲུལ་རིག་མཐོ་རིམ་སློབ་གྲྭ',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(TEXT_COLOR),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 40),
+                // CST photo
+                Container(
+                  width: 80,
+                  height: 80,
+                  child: Image.asset('assets/images/CST_logo.png'),
+                ),
+              ],
+            ),
+          ),
+          const Divider(
+            color: Colors.blue,
+            thickness: 2,
+            indent: 1,
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
--- lib/views/Teacher/TeacherHome.dart
+++ lib/views/Teacher/TeacherHome.dart
@@ -19,272 +19,127 @@ class _MyHomePageState extends State<MyHomePage> {
     ViewResultScreen(),
   ];
 
+  // Define a list of titles for each page.
+  final List<String> _pageTitles = [
+    'Result Processing System',
+    'Upload Result',
+    'View Result',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: NavBar(),
       appBar: AppBar(
-        backgroundColor: Colors.blue, // Change this color to blue
-        title: const Text('Result Processing System'),
+        backgroundColor: Colors.blue,
+        title: Text(_pageTitles[
+            _currentIndex]), // Dynamic title based on the selected index
         titleTextStyle: const TextStyle(
           color: Colors.white,
           fontSize: 20,
         ),
       ),
-      body: _pages[_currentIndex], // Display the selected page
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.blue,
-        unselectedItemColor: Color.fromARGB(255, 12, 241, 126),
-        selectedItemColor: Color.fromARGB(255, 245, 247, 245),
-        currentIndex: _currentIndex,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.upload),
-            label: 'Upload Result',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.remove_red_eye),
-            label: 'View Result',
-          ),
-        ],
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index; // it can change the selected page index
-          });
-        },
+      body: _pages[_currentIndex],
+      bottomNavigationBar: ClipRRect(
+        // Wrap the BottomNavigationBar with ClipRRect
+        borderRadius: const BorderRadius.vertical(
+            top: Radius.circular(25.0)), // Adjust the border radius as needed
+        child: BottomNavigationBar(
+          backgroundColor: Colors.blue,
+          selectedItemColor: Color.fromARGB(255, 245, 247, 245),
+          currentIndex: _currentIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.upload),
+              label: 'Upload Result',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.remove_red_eye),
+              label: 'View Result',
+            ),
+          ],
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
       ),
     );
   }
 }
 
 //************************************  Profile Screen starts  ************************************/
+
 class ProfileScreen extends StatelessWidget {
+  // Create a list of module data
+  final List<Module> modules = [
+    Module('CTE306', 'Fifth Semester/csdd'),
+    Module('Another Module', 'Description of another module'),
+    Module('CTE306', 'Fifth Semester/csdd'),
+    Module('Another Module', 'Description of another module'),
+    Module('CTE306', 'Fifth Semester/csdd'),
+    Module('Another Module', 'Description of another module'),
+    // Add more modules as needed
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: new AppBar(
-      //   title: new Text("Chat Item"),
-      //   backgroundColor: Colors.lightGreen,
-      // ),
       body: Column(
         children: [
-          // ChatItemScreen(),
-          // Module(),
-          DetailsItem(),
           const Divider(
             color: Colors.blue,
             thickness: 2,
             indent: 1,
           ),
           const Text(
-            'List of Module you are currently teaching',
+            'List of Modules you are currently teaching',
             style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
           ),
           Expanded(
             child: ListView(
-              children: <Widget>[
-                //1
-                Card(
-                  child: Container(
-                    margin: const EdgeInsets.only(
-                        left: 1), // Add a left margin of 15 pixels
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        left: BorderSide(
-                          color: Colors.blue, // Set the border color to blue
-                          width: 45, // Set the border width to 15 pixels
-                        ),
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        ListTile(
-                          leading: Icon(Icons.message),
-                          title: Text('CTE306'),
-                          subtitle: Text('Fifth Semester/csdd'),
-                          trailing: InkWell(
-                            onTap: () {
-                              // Navigate to the new page here when the trailing icon is tapped.
-                              // Navigator.push(
-                              //   context,
-                              //   MaterialPageRoute(
-                              //       builder: (context) => YourNewPage()),
-                              // );
-                              print("Next page");
-                            },
-                            child: Icon(Icons.arrow_right),
+              children: modules.map((module) {
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(
+                      1, 0, 8, 0), // Add right-side padding
+                  child: Card(
+                    color: Colors.white,
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 1),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          left: BorderSide(
+                            color: Colors.blue,
+                            width: 40,
                           ),
                         ),
-                      ],
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          ListTile(
+                            leading: Icon(Icons.message),
+                            title: Text(module.name),
+                            subtitle: Text(module.description),
+                            trailing: InkWell(
+                              onTap: () {
+                                // Handle module item click here
+                                print("Next page");
+                              },
+                              child: Icon(Icons.arrow_forward_ios_outlined),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-
-                //2
-                Card(
-                  color: Color.fromARGB(255, 231, 233, 233),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(Icons.message),
-                        title: Text('CTE306'),
-                        subtitle: Text('Fifth Semester/csdd'),
-                        trailing: InkWell(
-                          onTap: () {
-                            // Navigate to the new page here when the trailing icon is tapped.
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //       builder: (context) => YourNewPage()),
-                            // );
-                            print("Next page");
-                          },
-                          child: Icon(Icons.arrow_right),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                //3
-                Card(
-                  color: Color.fromARGB(255, 231, 233, 233),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(Icons.message),
-                        title: Text('CTE306'),
-                        subtitle: Text('Fifth Semester/csdd'),
-                        trailing: InkWell(
-                          onTap: () {
-                            // Navigate to the new page here when the trailing icon is tapped.
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //       builder: (context) => YourNewPage()),
-                            // );
-                            print("Next page");
-                          },
-                          child: Icon(Icons.arrow_right),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                //4
-                Card(
-                  color: Color.fromARGB(255, 231, 233, 233),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(Icons.message),
-                        title: Text('CTE306'),
-                        subtitle: Text('Fifth Semester/csdd'),
-                        trailing: InkWell(
-                          onTap: () {
-                            // Navigate to the new page here when the trailing icon is tapped.
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //       builder: (context) => YourNewPage()),
-                            // );
-                            print("Next page");
-                          },
-                          child: Icon(Icons.arrow_right),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                //5
-                Card(
-                  color: Color.fromARGB(255, 231, 233, 233),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(Icons.message),
-                        title: Text('CTE306'),
-                        subtitle: Text('Fifth Semester/csdd'),
-                        trailing: InkWell(
-                          onTap: () {
-                            // Navigate to the new page here when the trailing icon is tapped.
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //       builder: (context) => YourNewPage()),
-                            // );
-                            print("Next page");
-                          },
-                          child: Icon(Icons.arrow_right),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                //6
-                Card(
-                  color: Color.fromARGB(255, 231, 233, 233),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(Icons.message),
-                        title: Text('CTE306'),
-                        subtitle: Text('Fifth Semester/csdd'),
-                        trailing: InkWell(
-                          onTap: () {
-                            // Navigate to the new page here when the trailing icon is tapped.
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //       builder: (context) => YourNewPage()),
-                            // );
-                            print("Next page");
-                          },
-                          child: Icon(Icons.arrow_right),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                //7
-                Card(
-                  color: Color.fromARGB(255, 231, 233, 233),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(Icons.message),
-                        title: Text('CTE306'),
-                        subtitle: Text('Fifth Semester/csdd'),
-                        trailing: InkWell(
-                          onTap: () {
-                            // Navigate to the new page here when the trailing icon is tapped.
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //       builder: (context) => YourNewPage()),
-                            // );
-                            print("Next page");
-                          },
-                          child: Icon(Icons.arrow_right),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                // Add more ListTiles as needed
-              ],
+                );
+              }).toList(),
             ),
           ),
         ],
@@ -293,119 +148,13 @@ class ProfileScreen extends StatelessWidget {
   }
 }
 
-class DetailsItem extends StatelessWidget {
-  final leftSection = Container(
-    child: const CircleAvatar(
-      backgroundImage: NetworkImage(
-          "https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"),
-      backgroundColor: Colors.lightGreen,
-      radius: 40.0,
-    ),
-  );
+class Module {
+  final String name;
+  final String description;
 
-  //second
-  final middleSection = Expanded(
-    child: Container(
-      padding: const EdgeInsets.only(left: 15.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Container(
-            width: double
-                .infinity, // Set the width to occupy the entire available space
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.blue, width: 1.0),
-                borderRadius: BorderRadius.circular(10.0)),
-            child: const Card(
-              child: Text(
-                "Pema Galey",
-                style: TextStyle(
-                  color: Colors.black87,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 0.5),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.blue, width: 1.0),
-                borderRadius: BorderRadius.circular(10.0)),
-            child: const Card(
-              child: Text(
-                "TeacherID:",
-                style: TextStyle(color: Colors.black, fontSize: 16.0),
-              ),
-            ),
-          ),
-          SizedBox(height: 0.5),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.blue, width: 1.0),
-                borderRadius: BorderRadius.circular(10.0)),
-            child: const Card(
-              child: Text(
-                "College of Science and Technology",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 0.5),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.blue, width: 1.0),
-                borderRadius: BorderRadius.circular(10.0)),
-            child: const Card(
-              child: Text(
-                "Lecturer",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 0.5),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.blue, width: 1.0),
-                borderRadius: BorderRadius.circular(10.0)),
-            child: const Card(
-              color: Colors.white,
-              child: Text(
-                "pemagalax123gmail.com",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 3.0, horizontal: 8.0),
-      height: 180.0,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[leftSection, middleSection],
-      ),
-    );
-  }
+  Module(this.name, this.description);
 }
+
 //************************************  Profile Screen  ends ************************************/
 
 //************************************  Upload Screen  starts ************************************/
