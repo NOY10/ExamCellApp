33eb33dd995ed336d9e8ea1480367aa324634128
dhulpari <depashna19@gmail.com>
2023-11-08 11:24:25 +0530

    About

--- lib/views/NavBar/Aboutpage.dart
+++ lib/views/NavBar/Aboutpage.dart
@@ -1,15 +1,133 @@
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
-  const AboutPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('About App'),
+        title: Text('About Page'),
         centerTitle: true,
         backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Card(
+              elevation: 5,
+              color: Colors.blue,
+              margin: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Result Processing System',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 5,
+              color: Colors.white,
+              margin: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'This system helps the students with quicker access to the result and also helps the teachers to reduce their manual work; avoiding time consumption.',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 5,
+              color: Colors.white,
+              margin: EdgeInsets.only(top: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'This application provides a group of services in collaboration with:',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.all(0),
+                      leading: Text('•'),
+                      title: Text(
+                        'Exam Cell at College of Science and Technology',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 5,
+              color: Colors.white,
+              margin: EdgeInsets.only(top: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    ExpansionTile(
+                      title: Text(
+                        'Developers',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                      children: [
+                        Text(
+                          'This application is developed by:',
+                          textAlign: TextAlign.justify,
+                        ),
+                        Text('Tshering Norphel', textAlign: TextAlign.justify),
+                        Text('Phurpa Tshering', textAlign: TextAlign.justify),
+                        Text('Lobzang Yoenten', textAlign: TextAlign.justify),
+                        Text('Depashna Pradhan', textAlign: TextAlign.justify),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/views/NavBar/Helppage.dart
+++ lib/views/NavBar/Helppage.dart
@@ -1,16 +1,112 @@
 import 'package:flutter/material.dart';
 
 class HelpPage extends StatelessWidget {
-  const HelpPage({super.key});
+  const HelpPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // drawer: NavigationDrawerWidget(), //enable it if you want to have the drawer in the page
       appBar: AppBar(
         title: Text('Help'),
         centerTitle: true,
         backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Card(
+              elevation: 5,
+              margin: EdgeInsets.only(bottom: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Tutor Page',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.justify, // Justify the text
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'In this page only the tutor will be able to login with their required credentials. The tutor will be able to view his profile, upload the result and view the result.',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.justify, // Justify the text
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 5,
+              margin: EdgeInsets.only(bottom: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Student Page',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.justify, // Justify the text
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'In this page the students will be able to access their results. They will also be able to view their profiles.',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.justify, // Justify the text
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 5,
+              margin: EdgeInsets.only(bottom: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Exam Cell Page',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.justify, // Justify the text
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'In this page the exam cell will able Update the student records and also they will be able to declare the results after the compilation of the results.',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.justify, // Justify the text
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
